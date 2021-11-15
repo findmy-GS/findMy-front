ceb361c19adee5e5102186a0dcf4c512768fd574
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-11-16 08:31:50 +0900

    login_v1

--- find-my/src/screens/Login.js
+++ find-my/src/screens/Login.js
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
+import Google from "../assets/google.svg";
+import Naver from "../assets/naverpng.png";
+import Kakao from "../assets/kakao.svg";
+
 const AuthLayout=styled.div`
   display: flex;
   height: 100vh;
@@ -15,7 +19,10 @@ const Wrapper=styled.div`
   
 `;
 const Title=styled.h1`
- 
+ font-weight:600;
+ font-size:20px;
+ margin-bottom:10px;
+
 `;
 const FormBox=styled.div`
   
@@ -25,7 +32,8 @@ const FormBox=styled.div`
   align-items: center;
   background-color:white;
   padding:20px 0;
- 
+  border-radius:10px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   form{
     width:100%;
     display:flex;
@@ -37,6 +45,10 @@ const FormBox=styled.div`
 `;
 const ServiceName=styled.h1`
  text-align: center;
+ font-family: 'Hi Melody', cursive;
+ font-size:60px;
+ margin-bottom:20px;
+color: #74b9ff;
 `;
 
 
@@ -55,7 +67,7 @@ const FormBtn=styled(InputBase)`
   text-align: center;
   background-color: #74b9ff;
   border-radius: 5px;
-  margin-top:10px;
+  margin-top:20px;
   color:white;
   font-weight:600;
  
@@ -67,22 +79,33 @@ const SignUpBtn=styled.button`
   padding:8px;
   border:2px solid #74b9ff;
   border-radius: 5px;
-  margin-top:10px;
+  margin:10px 0px;
   text-align:center;
 
 
 `;
-const SLink=styled(Link)`
-  width:60%;
-  margin-top:10px;
-  text-align:end;
-  font-size:10px;
+const Links=styled.div`
+  display:flex;
+  font-size:12px;
+  margin:10px 0px 15px;
+  div{
+    margin:0px 5px;
+    width:2px;
+    background-color:gray;
+  }
 `;
 
-const Span=styled.span`
-  margin-top:10px;
-`
 
+const IconBox=styled.div`
+  margin-top:10px;
+  display:flex;
+  img{
+    width:30px;
+    height:30px;
+    margin-right:10px;
+  }
+
+`;
 function Login(){
   return (
     <AuthLayout>
@@ -93,12 +116,23 @@ function Login(){
           <form>
           <Input type="text" placeholder="아이디를 입력해 주세요."/>
           <Input type="password" placeholder="비밀번호를 입력해 주세요."/>
-          <SLink to="#">아이디 또는 비밀번호 찾기</SLink>
           <FormBtn type="submit" value="로그인"/>
-          <SignUpBtn>회원가입</SignUpBtn>
+         
           </form>
-          <Span>SNS 로그인</Span>    
+          <Links>
+            <Link to="#">아이디 찾기</Link>
+            <div></div>
+            <Link to="#">비밀번호 찾기</Link>
+            <div></div>
+            <Link to="#">회원가입</Link>
+          </Links>
+          <span>SNS 로그인</span> 
+          <IconBox>  
           
+              <img src={Google}/>
+              <img src={Naver}/>
+              <img src={Kakao}/>
+          </IconBox>  
         </FormBox>
       </Wrapper>
     </AuthLayout>
