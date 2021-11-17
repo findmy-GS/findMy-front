623dedbbaf50ccb290d1d3544ba021aedca6a7a0
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-11-17 15:05:45 +0900

    Update Home.js

--- find-my/src/screens/Home.js
+++ find-my/src/screens/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React,{useEffect} from "react";
+
+import styled from "styled-components";
+import KakaoMapScript from '../mapApi';
+
+const Container=styled.div`
+   width:50px;
+   height:400px;
+`;
+
+
 
 function Home(){
-  return <h1> Home</h1>;
+
+  useEffect(()=>{
+    KakaoMapScript();
+  },[]);
+  return (
+    <Container>
+
+    </Container>
+  );
 }
 
 export default  Home;
